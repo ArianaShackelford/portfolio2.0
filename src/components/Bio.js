d1e234f74f0751cfc966e75ff4182f16acd160fd
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-28 17:38:31 -0500

    set up and rearraged file structure

---
+++ src/components/Bio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Bio(){
+    return(
+        <div>
+            <h1>Who am I?</h1>
+            <p>my bio paragraph</p>
+            <img src='#' alt='profile image'/>
+        </div>
+    )
+}
